75d6f69ba2a057813a840898b9fa68d8f83fffc2
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-12 22:34:11 +0800

    Import QueryClient and QueryClientProvider from react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { Provider } from 'react-redux'
-import { store } from './store.ts'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes.tsx'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient } from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-    </Provider>
+    </QueryClientProvider>
   </StrictMode>,
 )
