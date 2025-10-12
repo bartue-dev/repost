5b0a9465e5ee3f6277caff6aeb445890e75a9464
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-12 22:33:23 +0800

    Comment out its code for the implementation of better auth

--- src/components/common/RequiredAuth.tsx
+++ src/components/common/RequiredAuth.tsx
@@ -1,14 +1,11 @@
-import { useLocation, Navigate, Outlet } from "react-router-dom";
-import { useAppSelector } from "../../features/hooks";
-import { selectCurrentToken } from "../../features/authSlice";
+// import { useLocation, Navigate, Outlet } from "react-router-dom";
 
-export default function RequiredAuth() {
-  const token = useAppSelector(selectCurrentToken)
-  const location = useLocation();
+// export default function RequiredAuth() {
+//   const location = useLocation();
 
-  return(
-    token
-      ? <Outlet/>   
-      : <Navigate to="/login" state={{from: location}} replace />
-  ) 
-}
+//   return(
+//     token
+//       ? <Outlet/>   
+//       : <Navigate to="/login" state={{from: location}} replace />
+//   ) 
+// }
