1963106e69cd7f7250eca66987b21dd995305477
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-12 22:34:32 +0800

    Comment out some code then import the some components for testing

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
-import Login from "./components/Auth/Login";
-import Home from "./components/Pages/Home";
-import RequiredAuth from "./components/common/RequiredAuth";
-import Welcome from "./components/Pages/Welcome";
-import PersistLogin from "./components/common/PersistLogin";
+import Home from "./components/pages/home";
+// import RequiredAuth from "./components/common/RequiredAuth";
+// import Welcome from "./components/pages/Welcome";
+// import PersistLogin from "./components/common/PersistLogin";
+import SignUp from "./components/auth/sign-up";
+import SignIn from "./components/auth/sign-in";
 
 export const router = createBrowserRouter([
   {
@@ -14,20 +15,18 @@ export const router = createBrowserRouter([
       {index: true, element: <Home/>},
       {path: "home", element: <Home/>},
 
-      {element: <PersistLogin/>,
-        children: [
-          {element: <RequiredAuth />,
-            children: [
-              {path: "welcome", element: <Welcome />}
-            ]
-          }
-        ]
-      }
+      // {element: <PersistLogin/>,
+      //   children: [
+      //     {element: <RequiredAuth />,
+      //       children: [
+      //         {path: "welcome", element: <Welcome />}
+      //       ]
+      //     }
+      //   ]
+      // }
       
     ]
   },
-  { 
-    path: "login",
-    element: <Login/>
-  }
+  { path: "sign-up", element: <SignUp /> },
+  { path: "sign-in", element: <SignIn/> }
 ])
