13c32769587613162c4b939ad5fe39d9bc35251f
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-12 23:09:43 +0800

    Creates a publicRoute that able to restrict the logged in user to navigate to sign in and sign up page

---
+++ src/components/common/publicRoute.tsx
@@ -0,0 +1,12 @@
+import { useUserData } from "@/hooks/use-user-data";
+import { Outlet, Navigate } from "react-router-dom";
+
+export default function PublicRoute() {
+  const { session } = useUserData();
+
+  if (session?.user) {
+    return <Navigate to="/welcome" replace/>
+  }
+
+  return <Outlet/>
+}
