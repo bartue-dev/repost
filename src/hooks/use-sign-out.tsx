22718a9f19676fb7f77e4c00e046ca6cb422f01a
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-15 21:35:30 +0800

    Import useUserData the get the refetch method so every time useSignOut is call the session data will refetch

--- src/hooks/use-sign-out.tsx
+++ src/hooks/use-sign-out.tsx
@@ -1,14 +1,17 @@
 import { authClient } from "@/lib/auth-client";
 import { useNavigate } from "react-router-dom";
+import { useUserData } from "./use-user-data";
 
 export default function useSignOut() {
   const navigate = useNavigate();
+  const { refetch } = useUserData();
 
   const signOut = async () => {
 
     try {
       const response = await authClient.signOut()
 
+      refetch();
       navigate("/sign-in");
 
       console.log("SIGN OUT:", response)
