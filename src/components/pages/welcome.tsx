e8b2f8baa6b3aa95e5fa711f9069f6d6cca2cf99
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-15 21:34:45 +0800

    Import useUserData and explore this session data

--- src/components/pages/welcome.tsx
+++ src/components/pages/welcome.tsx
@@ -1,8 +1,11 @@
 import { Button } from "../ui/button"
 import useSignOut from "@/hooks/use-sign-out"
+import { useUserData } from "@/hooks/use-user-data";
 
 export default function Welcome(){
   const signOut = useSignOut();
+  const {session} = useUserData();
+  const user = session?.user;
 
   const handleSignOut = async () => {
     await signOut()
@@ -10,7 +13,7 @@ export default function Welcome(){
 
   return (
     <div>
-      <h1>WELCOME!</h1>
+      <h1>WELCOME! {user?.name} </h1>
       <Button
         variant="outline"
         onClick={handleSignOut}
