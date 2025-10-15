69471f655ed578853c490c079a2022d9330d29fc
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-15 21:38:14 +0800

    Added fetchOptions credential: true

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,9 @@
 import { createAuthClient } from "better-auth/react"
 
+//better auth invocation
 export const authClient = createAuthClient({
   baseURL: import.meta.env.VITE_BASE_URL,
+  fetchOptions: {
+    credentials: "include"
+  },
 })
