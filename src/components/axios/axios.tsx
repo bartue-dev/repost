35c45198abc16dbac900eb69f2b68b6420844379
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-18 15:54:08 +0800

    Creates an axiosInstance that able to query public queries then axiosPrivate that query private queries

---
+++ src/components/axios/axios.tsx
@@ -0,0 +1,13 @@
+import axios from "axios"
+
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL
+})
+
+export const axiosPrivate = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
