4984e99f18c4fbd33e329e8b429e41c9938a51ca
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-19 21:35:53 +0800

    Added flex flex-col min-h-screen bg-gray-100 to the App component parent container

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import Navbar from "./components/common/navbar"
 function App() {
 
   return (
-   <div className="h-screen">
+   <div className="flex flex-col min-h-screen bg-gray-100">
       {/* Navbar */}
       <div>
         <Navbar/>
       </div>
 
       {/* App child pages */}
-      <div className="p-5 ">
+      <div className="p-5">
         <Outlet/>
       </div>
 
