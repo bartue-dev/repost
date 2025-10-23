3edfd6a8ac3f1bba80d142608367287ef286605e
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-23 23:52:35 +0800

    Render tags and added a bookmark icon then comments each container to be more readable

--- src/components/common/posts.tsx
+++ src/components/common/posts.tsx
@@ -1,5 +1,5 @@
 
-import { LoaderCircle } from "lucide-react"
+import { Bookmark, LoaderCircle } from "lucide-react"
 import { format, formatDistanceToNow } from "date-fns"
 import type { PostsTypes } from "@/lib/types"
 // import DOMPurify from "dompurify"
@@ -47,14 +47,16 @@ export default function Posts({
   }
 
   return (
-    <div className="grid grid-cols-1 gap-3 mt-5">
+    <div className="grid grid-cols-2 gap-3 mt-5 overflow-auto">
       {
       posts?.length === 0
-      ?
+      ? //if no post found
         <div>
-          <p className="text-center italic">Sorry {searchPost.join(" ")} not found</p>
+          <p className="text-center italic">
+            Sorry <span className="font-bold">{searchPost.join(" ")}</span> not found
+          </p>
         </div>
-      : 
+      : //render posts found
       posts?.map(post => {
         // const sanitizePosts = DOMPurify.sanitize(post.content);
         const formatedDate = format(new Date(post.createdAt), "MMM dd")
@@ -63,8 +65,9 @@ export default function Posts({
         return (
           <div 
             key={post.id}
-            className="rounded-sm p-5 bg-white shadow-sm flex flex-col gap-4 group hover:cursor-pointer"
+            className="rounded-sm p-5 bg-white shadow-sm space-y-4"
           >
+            {/* profile information */}
             <div className="flex items-center justify-start gap-2">
               <div
                   className="rounded-full w-12 h-12 bg-gray-100 flex items-center justify-center text-base font-bold"
@@ -77,45 +80,62 @@ export default function Posts({
               </div>
             </div>
 
+            {/* title */}
             <h2 
-              className="text-2xl break-words ml-10 font-montserrat font-bold text-gray-800 group-hover:text-blue-600"
+              className="text-2xl break-words ml-10 font-montserrat font-bold text-gray-800 hover:text-blue-600 cursor-pointer"
             >
               {post.title.substring(0, 50)}{post.title.length > 50 && "..."} 
             </h2>
 
-            <div className="flex items-center gap-6 text-sm text-gray-800">
-              <div className="flex ml-10">
-                {reactionList.map(list => {
-                  //find the reaction type to sort the rendered emoji base on reactionList order
-                  const reactionIsExist = post.reactions.find(reaction => reaction.type === list.type);
-                  if (!reactionIsExist) return null;
+            {/* tags and bookmark */}
+
+              <div className="flex items-center gap-2 ml-10">
+                {post.tags.map(tag => (
+                  <div
+                    key={tag}
+                  >
+                    <span className="text-gray-700 text-sm"># {tag}</span>
+                  </div>
+                ))}
+              </div> 
+
+            {/* reactions */}
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-6 text-sm text-gray-800">
+                <div className="flex ml-10">
+                  {reactionList.map(list => {
+                    //find the reaction type to sort the rendered emoji base on reactionList order
+                    const reactionIsExist = post.reactions.find(reaction => reaction.type === list.type);
+                    if (!reactionIsExist) return null;
+                    
+                    return (
+                      <div 
+                        role="img"
+                        key={list.type}
+                        className={`bg-gray-100 flex items-center justify-center w-6 h-6 rounded-full p-4 -mr-4
+                          ${list.zIndex === 50 ? "z-50" :
+                            list.zIndex === 40 ? "z-40" :
+                            list.zIndex === 30 ? "z-30" :
+                            list.zIndex === 20 ? "z-20" :
+                            ""
+                          }`}
+                      >
+                        {list.emoji}
+                      </div>
+                    )
+                  })}
+                </div>
+                {post.reactions.length > 0 
+                  &&
+                  <span 
+                    className="text-sm text-gray-700"
+                  >
+                    {post.reactions.length} reactions
+                  </span>
+                }
                   
-                  return (
-                    <div 
-                      role="img"
-                      key={list.type}
-                      className={`bg-gray-100 flex items-center justify-center w-6 h-6 rounded-full p-4 -mr-4
-                        ${list.zIndex === 50 ? "z-50" :
-                          list.zIndex === 40 ? "z-40" :
-                          list.zIndex === 30 ? "z-30" :
-                          list.zIndex === 20 ? "z-20" :
-                          ""
-                        }`}
-                    >
-                      {list.emoji}
-                    </div>
-                  )
-                })}
               </div>
-              {post.reactions.length > 0 
-                &&
-                <span 
-                  className="text-sm text-gray-700"
-                >
-                  {post.reactions.length} reactions
-                </span>
-              }
-                
+              <Bookmark />
             </div>
 
             {/* <div
