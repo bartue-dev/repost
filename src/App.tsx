942108631b52092eaec592c4786379722246a3f7
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-23 23:51:11 +0800

    Do not render Navbar if url is create-post

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "./components/common/navbar"
+import { useLocation } from "react-router-dom"
 
 function App() {
+  const location = useLocation();
+
+  const pathname = location.pathname.split("/")[1];
 
   return (
    <div className="flex flex-col min-h-screen bg-gray-100">
       {/* Navbar */}
-      <div>
-        <Navbar/>
-      </div>
+
+      {pathname !== "create-post"
+        &&
+          <div>
+            <Navbar/>
+          </div>
+      }
 
       {/* App child pages */}
       <div className="p-5">
