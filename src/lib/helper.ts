cf2bd4247d4bace3e24b876b74b8fc1c97113a50
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-25 20:14:28 +0800

    Creates a helper file. This where helper function stores

---
+++ src/lib/helper.ts
@@ -0,0 +1,7 @@
+//list of reaction to be rendered
+export const reactionList = [
+  {type: "LOVED", emoji: "❤️", zIndex: 50},
+  {type: "LIKED", emoji: "👍", zIndex: 40},
+  {type: "COOL", emoji: "😎", zIndex: 30},
+  {type: "APPLAUSE", emoji: "👏", zIndex: 20},
+]
