888877514a5a3a30f8fd72b54b101d5fb9a82196
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-29 23:20:18 +0800

    Creates a deleteComment mutation query. As well as pass the commentId in the Link as params so the EditComment Component uses this commentId to query the specific comment and able to render it

--- src/components/common/comment-actions.tsx
+++ src/components/common/comment-actions.tsx
@@ -6,48 +6,85 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { Ellipsis } from "lucide-react"
 import { useUserData } from "@/hooks/use-user-data";
+import { Link } from "react-router-dom";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { axiosPrivate } from "../axios/axios";
+import type { ApiErr } from "@/lib/types";
 
 type CommentActionPropsTypes = {
   commentUserId: string,
-  postUserId: string
+  postUserId: string,
+  commentId: string,
+  postId: string
 }
 
+//CommentActions component
 export default function CommentActions({
   commentUserId, 
-  postUserId
+  postUserId,
+  commentId,
+  postId
 } : CommentActionPropsTypes) {
   const { session } = useUserData();
   const user = session?.user;
+  const queryClient = useQueryClient();
 
-  console.log("USER:", user)
+  const {
+    mutate: deleteComment
+  } = useMutation({
+    mutationFn: async () => {
+      const response = await axiosPrivate.delete(`/v1/api/comment/${commentId}`)
+      return response
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ["post", postId]})
+    },
+    onError: (error: ApiErr) => {
+      console.error("Delet comment error:", error)
+    }
+  });
+
+  const handleDeleteComment = () => {
+    deleteComment();
+  }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger 
-        asChild
-        className="border-none outline-none"
-      >
-        <button
-          className="cursor-pointer hover:bg-gray-100 p-1 rounded-md"
+    <div>
+      <DropdownMenu>
+        <DropdownMenuTrigger 
+          asChild
+          className="border-none outline-none"
         >
-          <Ellipsis
-            className="text-gray-700"
-          />
-        </button>
-      </DropdownMenuTrigger>
-      {
-        commentUserId === user?.id ||
-        postUserId === user?.id
-        ? // if comment author and post user is equal to current user
-        <DropdownMenuContent>
-          <DropdownMenuItem>Edit</DropdownMenuItem>
-          <DropdownMenuItem>Delete</DropdownMenuItem>
-        </DropdownMenuContent>
-        : // otherwise
-        <DropdownMenuContent>
-          <DropdownMenuItem>Copy</DropdownMenuItem>
-        </DropdownMenuContent>
-      }
-    </DropdownMenu>
+          <button
+            className="cursor-pointer hover:bg-gray-100 p-1 rounded-md"
+          >
+            <Ellipsis
+              className="text-gray-700"
+            />
+          </button>
+        </DropdownMenuTrigger>
+        {
+          commentUserId === user?.id ||
+          postUserId === user?.id
+          ? // if comment author and post author is equal to current user
+          <DropdownMenuContent>
+            <DropdownMenuItem>
+              <Link to={`/edit-comment/${commentId}/`} >
+                Edit
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={handleDeleteComment}
+            >
+              Delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+          : // otherwise render a copy only option
+          <DropdownMenuContent>
+            <DropdownMenuItem>Copy</DropdownMenuItem>
+          </DropdownMenuContent>
+        }
+      </DropdownMenu>
+    </div>
   )
 }
