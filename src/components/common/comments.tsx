534e1b14a0e39b7708fc34dab61b41ad7d2d098c
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-10-30 23:06:56 +0800

    Added a isReplyBtn state that conditionally render ChildCommentForm if equal to index. Then added a CommentAction to rendered child comments

--- src/components/common/comments.tsx
+++ src/components/common/comments.tsx
@@ -11,11 +11,14 @@ import type { ApiErr, CommentPropsType } from "@/lib/types";
 import CommentTextEditor from "./comment-text-editor";
 import DOMPurify  from "dompurify";
 import CommentActions from "./comment-actions";
+import { useState } from "react";
+import ChildCommentForm from "./child-comment";
 
 type CommentData = z.infer<typeof CreateCommentSchema>;
 
 //Comment Component
 export default function Comments({post, setIsCommentAdded} : CommentPropsType ) {
+  const [isReplyBtn, setIsReplyBtn] = useState<number | undefined>(undefined)
   const queryClient = useQueryClient(); // useQueryClient is use for global invalidation
   const navigate = useNavigate();
   const location = useLocation();
@@ -42,8 +45,7 @@ export default function Comments({post, setIsCommentAdded} : CommentPropsType ) 
 
       return response;
     },
-    onSuccess: (response) => {
-      console.log("Add comment response",response);
+    onSuccess: () => {
       setIsCommentAdded(true)
       queryClient.invalidateQueries({queryKey: ["post", post?.id]});
       reset()
@@ -106,7 +108,7 @@ export default function Comments({post, setIsCommentAdded} : CommentPropsType ) 
       
       </div>
       <div className="space-y-5 mt-8">
-        {post?.comment.map(c => {
+        {post?.comment.map((c, i) => {
           
         const sanitizeComment = DOMPurify.sanitize(c.comment);
 
@@ -126,8 +128,16 @@ export default function Comments({post, setIsCommentAdded} : CommentPropsType ) 
                   className="border p-3 rounded-sm space-y-2 w-226 relative"
                 >
                   <div className="flex gap-2">
-                    <div className="font-sofia-sans font-semibold">{c.user.name} &bull;</div>
-                    <div className="font-sofia-sans text-gray-800">{format(new Date(c.createdAt), "MMM dd")}</div>
+                    <div 
+                      className="font-sofia-sans font-semibold"
+                    >
+                      {c.user.name} &bull;
+                    </div>
+                    <div 
+                      className="font-sofia-sans text-gray-800"
+                    >
+                      {format(new Date(c.createdAt), "MMM dd")}
+                    </div>
                   </div>
                   <div>
                     <div
@@ -147,10 +157,23 @@ export default function Comments({post, setIsCommentAdded} : CommentPropsType ) 
               </div>
               {/* Reply button */}
               <button
-                className="cursor-pointer text-gray-800 text-sm font-sofia-sans ml-14"
+                className="cursor-pointer text-gray-800 text-sm font-sofia-sans ml-14 hover:underline"
+                onClick={() => setIsReplyBtn(i)}
               >
                 Reply
               </button>
+              {/* if isReplyBtn state is equal to i render ChildCommentForm */}
+              {
+                isReplyBtn === i
+                &&
+                  <div className="ml-20 mt-5">
+                    <ChildCommentForm
+                      setIsReplyBtn={setIsReplyBtn}
+                      commentId={c.id}
+                      postId={post.id}
+                    />
+                  </div>
+              }
             </div>
             {/* child comment */}
             <div className="space-y-5 mt-5 ml-12">
@@ -168,7 +191,7 @@ export default function Comments({post, setIsCommentAdded} : CommentPropsType ) 
                       >
                         {c.user.name.charAt(0)}
                       </div>
-                      <div className="border w-full p-3 rounded-sm space-y-2">
+                      <div className="border w-full p-3 rounded-sm space-y-2 relative">
                         <div className="flex gap-2">
                           <div 
                             className="font-sofia-sans font-semibold"
@@ -185,6 +208,15 @@ export default function Comments({post, setIsCommentAdded} : CommentPropsType ) 
                           <div
                             className="ql-editor"
                             dangerouslySetInnerHTML={{__html: sanitizeChildComment}}
+                          />
+                        </div>
+
+                        <div className="absolute right-5 top-2">
+                          <CommentActions
+                            commentUserId={cc.userId}
+                            postUserId={post.userId}
+                            commentId={cc.id}
+                            postId={post.id}
                           />
                         </div>
                       </div>
