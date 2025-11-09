89d033fa25c664b114edef04e850215a547ef2c1
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-11-09 23:36:53 +0800

    Create a EditUserInfoSchema, validates the name, email as well as the image file only specific image file is allowed

--- src/lib/zod-schema.ts
+++ src/lib/zod-schema.ts
@@ -14,6 +14,45 @@ export const SignInSchema = z.object({
   password: z.string().min(1, "Invalid password")
 });
 
+const MAX_FILE_SIZE = 510241024
+const ACCEPTED_IMAGE_TYPES = [
+'image/jpeg',
+'image/jpg',
+'image/png',
+'image/webp',
+]
+
+//signup
+export const EditUserInfoSchema = z.object({
+  name: z.string().min(1, "Name must not be empty"),
+  email: z.email("Invalid email"),
+  image: z
+    .any()
+    .optional()
+    .refine(
+      (files) => {
+        if (!files || files.length === 0) return true; // allow empty
+        return files.length === 1;
+      },
+      { message: "Only one image file is allowed." }
+    )
+    .refine(
+      (files) => {
+        if (!files || files.length === 0) return true;
+        return files[0].size <= MAX_FILE_SIZE;
+      },
+      { message: `Max file size is 5MB.` }
+    )
+    .refine(
+      (files) => {
+        if (!files || files.length === 0) return true;
+        return ACCEPTED_IMAGE_TYPES.includes(files[0].type);
+      },
+      { message: ".jpg, .jpeg, .png and .webp files are accepted." }
+    ),
+});
+
+
 //create post
 export const CreatePostSchema = z.object({
   title: z.string().trim().min(1, "Title must not be empty"),
